2fae67b84cf96a17fcfd761d2129298b0d674077
DHARAKA DM <deshandm699@gmail.com>
2024-02-28 10:08:55 +0530

    images added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,25 @@
+import menuimg from '../../img/menu-svgrepo-com.svg'
+import searchimg from '../../img/search-svgrepo-com.svg'
+import cartimg from '../../img/full-shoping-cart-svgrepo-com.svg'
+
 const Header = () => {
   return <header>
   {/* header left */}
+
+  <div>
+    <img src={menuimg} alt='menu'/>
+    <h2>DMD</h2>
+    <div>
+      <input type="text" placeholder="Search"/>
+      <img src={cartimg} alt='cart'/>
+    </div>
+  </div>
+
+
+
+
   {/* header right */}
+  <img src={searchimg} alt='search'/>
   </header>;
 };
 
