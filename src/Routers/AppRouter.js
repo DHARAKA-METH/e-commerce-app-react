ae62363c51924e466e08d940cda74eebcce10fcc
DHARAKA DM <deshandm699@gmail.com>
2024-02-28 15:59:55 +0530

    React router protected option

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -3,17 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import User from "../Pages/User/User";
 import Home from "../Pages/Home/Home";
+import Login from "../Login/Login";
+import UserProtected from "./UserProtected";
 
 const AppRouter = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainLayout/>}>
-          <Route index element={<Home/>}/>
-          <Route path="User" element={<User/>}/>
-
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route element={<UserProtected />}>
+              <Route path="User" element={<User />} />
+            </Route>
           </Route>
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
