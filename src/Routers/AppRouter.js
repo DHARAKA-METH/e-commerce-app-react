5dddcbcaa0dfeb3b9a1d8a86a6f313b9c6d12b91
DHARAKA DM <deshandm699@gmail.com>
2024-02-28 16:36:21 +0530

    router provider method(for routing another method)

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -1,26 +1,57 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import User from "../Pages/User/User";
 import Home from "../Pages/Home/Home";
 import Login from "../Login/Login";
 import UserProtected from "./UserProtected";
 
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<MainLayout/>,
+    children:[
+      {
+        index:true,
+        element:<Home/>
+      },
+      {
+        element:<UserProtected/>,
+        children:[
+          {
+            path:'user',
+            element:<User/>
+          }
+        ]
+      }
+      
+    ]
+
+},
+{
+  path:'login',
+  element:<Login/>
+}
+])
+
+
 const AppRouter = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route element={<UserProtected />}>
-              <Route path="User" element={<User />} />
-            </Route>
-          </Route>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <RouterProvider router={router}/>
+    // <div>
+    //   <BrowserRouter>
+    //     <Routes>
+    //       <Route path="/" element={<MainLayout />}>
+    //         <Route index element={<Home />} />
+    //         <Route element={<UserProtected />}>
+    //           <Route path="User" element={<User />} />
+    //         </Route>
+    //       </Route>
+    //       <Route path="/login" element={<Login />} />
+    //     </Routes>
+    //   </BrowserRouter>
+    // </div>
   );
 };
 
