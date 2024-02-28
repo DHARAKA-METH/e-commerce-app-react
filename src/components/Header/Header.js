c5c9899d63ddc574111c967a160157812d7a19dc
DHARAKA DM <deshandm699@gmail.com>
2024-02-28 10:51:38 +0530

    header js css design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,39 @@
-import menuimg from '../../img/menu-svgrepo-com.svg'
-import searchimg from '../../img/search-svgrepo-com.svg'
-import cartimg from '../../img/full-shoping-cart-svgrepo-com.svg'
+import menuimg from "../../img/menu-svgrepo-com.svg";
+import searchimg from "../../img/search-svgrepo-com.svg";
+import cartimg from "../../img/full-shoping-cart-svgrepo-com.svg";
 
 const Header = () => {
-  return <header>
-  {/* header left */}
+  return (
+    <header className="fixed top-0 left-0 z-[100] drop-shadow-header-shadow w-full p-5 bg-header-background flex items-center justify-between">
+      {/* header left */}
 
-  <div>
-    <img src={menuimg} alt='menu'/>
-    <h2>DMD</h2>
-    <div>
-      <input type="text" placeholder="Search"/>
-      <img src={cartimg} alt='cart'/>
-    </div>
-  </div>
+      <div className="flex items-center">
+        <img
+          src={menuimg}
+          alt="menu"
+          className="w-6 h-6 object-contain cursor-pointer"
+        />
+        <h2 className="ml-2 text-sm font-bold">
+          <span className="text-red-900">D</span>MD
+        </h2>
+        <div className="overflow-hidden ml-8 flex items-center rounded-full bg-[#fdf3f3]  ">
+          <input type="text" placeholder="Search" className="ml-1 outline-none p-2 font-semibold text-sm w-[200px] bg-inherit" />
+          <img
+            src={searchimg}
+            alt="cart"
+            className="mr-2 w-6 h-6 object-contain cursor-pointer"
+          />
+        </div>
+      </div>
 
-
-
-
-  {/* header right */}
-  <img src={searchimg} alt='search'/>
-  </header>;
+      {/* header right */}
+      <img
+        src={cartimg}
+        alt="search"
+        className="w-6 h-6 object-contain cursor-pointer"
+      />
+    </header>
+  );
 };
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "header-background": "#ffd9d9",
+      },
+      dropShadow: {
+        "header-shadow": "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+      },
+    },
   },
   plugins: [],
-}
+};
