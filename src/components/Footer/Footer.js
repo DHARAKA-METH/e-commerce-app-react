b90b7d36f5340edbc82365b3be8015c3fe18df79
DHARAKA DM <deshandm699@gmail.com>
2024-02-29 17:49:08 +0530

    hover efect

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,16 +5,25 @@ import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0  left-0 z-[100] drop-shadow-header-shadow w-full p-5 bg-header-background flex items-center justify-between py-3 px-2">
-      <IconButton className="flex flex-col  ">
+    <footer className="fixed bottom-0  left-0 z-[100] drop-shadow-header-shadow w-full p-5 bg-header-background flex items-center justify-between py-3 px-2 ">
+      <IconButton
+        className="flex flex-col hover:text-[#c82196] "
+        sx={{ color: "black" }}
+      >
         <HomeOutlinedIcon />
         <p className="font-bold font-sans text-sm ">Home</p>
       </IconButton>
-      <IconButton className="flex flex-col ">
+      <IconButton
+        className="flex flex-col hover:text-[#c82196]  "
+        sx={{ color: "black" }}
+      >
         <AppsOutlinedIcon />
         <p className="font-bold font-sans text-sm ">Category</p>
       </IconButton>
-      <IconButton className="flex flex-col ">
+      <IconButton
+        className="flex flex-col hover:text-[#c82196]  "
+        sx={{ color: "black" }}
+      >
         <AccountCircleOutlinedIcon />
         <p className="font-bold font-sans text-sm ">Profile</p>
       </IconButton>
