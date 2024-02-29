ae546a68db36e7cd98721db2f54ed2dae9b84787
DHARAKA DM <deshandm699@gmail.com>
2024-02-29 17:53:13 +0530

    color update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,21 +7,21 @@ const Footer = () => {
   return (
     <footer className="fixed bottom-0  left-0 z-[100] drop-shadow-header-shadow w-full p-5 bg-header-background flex items-center justify-between py-3 px-2 ">
       <IconButton
-        className="flex flex-col hover:text-[#c82196] "
+        className="flex flex-col hover:text-[#34a820] "
         sx={{ color: "black" }}
       >
         <HomeOutlinedIcon />
         <p className="font-bold font-sans text-sm ">Home</p>
       </IconButton>
       <IconButton
-        className="flex flex-col hover:text-[#c82196]  "
+        className="flex flex-col hover:text-[#34a820]  "
         sx={{ color: "black" }}
       >
         <AppsOutlinedIcon />
         <p className="font-bold font-sans text-sm ">Category</p>
       </IconButton>
       <IconButton
-        className="flex flex-col hover:text-[#c82196]  "
+        className="flex flex-col hover:text-[#34a820]  "
         sx={{ color: "black" }}
       >
         <AccountCircleOutlinedIcon />
