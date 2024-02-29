c02753de99efc33110236cddcccbe519cf028699
DHARAKA DM <deshandm699@gmail.com>
2024-02-29 17:03:00 +0530

    footer icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,25 @@
-
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import { IconButton } from "@mui/material";
+import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
 const Footer = () => {
   return (
-    <div >
-      Footer
-    </div>
-  )
-}
+    <footer className="fixed bottom-0  left-0 z-[100] drop-shadow-header-shadow w-full p-5 bg-header-background flex items-center justify-between py-3 px-2">
+      <IconButton className="flex flex-col  ">
+        <HomeOutlinedIcon />
+        <p className="font-bold font-sans text-sm ">Home</p>
+      </IconButton>
+      <IconButton className="flex flex-col ">
+        <AppsOutlinedIcon />
+        <p className="font-bold font-sans text-sm ">Category</p>
+      </IconButton>
+      <IconButton className="flex flex-col ">
+        <AccountCircleOutlinedIcon />
+        <p className="font-bold font-sans text-sm ">Profile</p>
+      </IconButton>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
