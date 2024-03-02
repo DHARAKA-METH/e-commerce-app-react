89f4b364adb971e43209c10f73180abf3419ada2
DHARAKA DM <deshandm699@gmail.com>
2024-03-02 09:31:53 +0530

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,12 +5,6 @@ import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined
 import { useState } from "react";
 
 const Footer = () => {
-  const icon = [
-    { icon: HomeOutlinedIcon, iconText: "Home" },
-    { icon: AppsOutlinedIcon, iconText: "Category" },
-    { icon: AccountCircleOutlinedIcon, iconText: "profile" },
-  ];
-
   const FooterIcon = ({ Icon, iconText }) => (
     <IconButton className="flex flex-col  " sx={{ color: "black" }}>
       <Icon />
@@ -19,15 +13,15 @@ const Footer = () => {
   );
 
   const [whoIsClick, setWhoIsClick] = useState({
-    home: true,
-    category: false,
-    profile: false,
+    Home: true,
+    Category: false,
+    Profile: false,
   });
   return (
     <footer className="fixed bottom-0  left-0 z-[100] drop-shadow-header-shadow w-full p-5 bg-header-background flex items-center justify-between py-3 px-2 ">
-      {icon.map(({ icon, iconText }, index) => (
-        <FooterIcon key={index} Icon={icon} iconText={iconText} />
-      ))}
+      <FooterIcon Icon={HomeOutlinedIcon} iconText="Home" />
+      <FooterIcon Icon={AppsOutlinedIcon} iconText="Category" />
+      <FooterIcon Icon={AccountCircleOutlinedIcon} iconText="Profile" />
     </footer>
   );
 };
