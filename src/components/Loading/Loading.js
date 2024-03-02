d3020f7485bb6f9c314713241a1d1dd6865f3803
DHARAKA DM <deshandm699@gmail.com>
2024-03-02 12:18:43 +0530

    add css as module

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,10 +1,11 @@
+import style from "./Loading.module.css";
 
 const Loading = () => {
   return (
     <div className="relative top-0 left-0 w-screen h-screen flex items-center justify-center bg-white z-[101]">
-      Loading
+      <div className={style.loading_dots}></div>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
