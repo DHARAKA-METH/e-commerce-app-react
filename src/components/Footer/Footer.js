4ba2f87b8615c9ef9dd0ed01c16e279e50719f5a
DHARAKA DM <deshandm699@gmail.com>
2024-03-02 09:27:31 +0530

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,31 +2,32 @@ import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import { IconButton } from "@mui/material";
 import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import { useState } from "react";
 
 const Footer = () => {
+  const icon = [
+    { icon: HomeOutlinedIcon, iconText: "Home" },
+    { icon: AppsOutlinedIcon, iconText: "Category" },
+    { icon: AccountCircleOutlinedIcon, iconText: "profile" },
+  ];
+
+  const FooterIcon = ({ Icon, iconText }) => (
+    <IconButton className="flex flex-col  " sx={{ color: "black" }}>
+      <Icon />
+      <p className="font-bold font-sans text-sm ">{iconText}</p>
+    </IconButton>
+  );
+
+  const [whoIsClick, setWhoIsClick] = useState({
+    home: true,
+    category: false,
+    profile: false,
+  });
   return (
     <footer className="fixed bottom-0  left-0 z-[100] drop-shadow-header-shadow w-full p-5 bg-header-background flex items-center justify-between py-3 px-2 ">
-      <IconButton
-        className="flex flex-col  "
-        sx={{ color: "black" }}
-      >
-        <HomeOutlinedIcon />
-        <p className="font-bold font-sans text-sm ">Home</p>
-      </IconButton>
-      <IconButton
-        className="flex flex-col   "
-        sx={{ color: "black" }}
-      >
-        <AppsOutlinedIcon />
-        <p className="font-bold font-sans text-sm ">Category</p>
-      </IconButton>
-      <IconButton
-        className="flex flex-col   "
-        sx={{ color: "black" }}
-      >
-        <AccountCircleOutlinedIcon />
-        <p className="font-bold font-sans text-sm ">Profile</p>
-      </IconButton>
+      {icon.map(({ icon, iconText }, index) => (
+        <FooterIcon key={index} Icon={icon} iconText={iconText} />
+      ))}
     </footer>
   );
 };
