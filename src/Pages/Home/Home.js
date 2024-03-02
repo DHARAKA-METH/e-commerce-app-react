dc32514c3e85b87169b4e7d75b7c6a4ccf2ed109
DHARAKA DM <deshandm699@gmail.com>
2024-03-02 12:11:54 +0530

    Loading components

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,12 @@
+import { useState } from 'react'
 import Ads from './Ads'
 import Products from './Products'
+import Loading from '../../components/Loading/Loading'
 
 const Home = () => {
+  const [loading,setLoading] =useState(true)
+
+  if(loading)return<Loading/>
   return (
     <div className="px-5 py-[100px]  w-full h-screen overflow-y-scroll">
       <Ads/>
---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+  return (
+    <div className="relative top-0 left-0 w-screen h-screen flex items-center justify-center bg-white z-[101]">
+      Loading
+    </div>
+  )
+}
+
+export default Loading
