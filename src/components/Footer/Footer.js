b36248c465366fd9ac2d8917c9f4e9e846ee2543
DHARAKA DM <deshandm699@gmail.com>
2024-03-02 10:39:14 +0530

    set current location in app by red line

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,12 +5,48 @@ import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined
 import { useState } from "react";
 
 const Footer = () => {
-  const FooterIcon = ({ Icon, iconText }) => (
-    <IconButton className="flex flex-col  " sx={{ color: "black" }}>
-      <Icon />
-      <p className="font-bold font-sans text-sm ">{iconText}</p>
-    </IconButton>
-  );
+  const FooterIcon = ({ Icon, iconText, clickFun }) => {
+    const obj = {
+      Home: true,
+      Category: false,
+      Profile: false,
+    };
+
+    const handleClick = () => {
+      if (iconText === "Category") {
+        obj.Home = false;
+        obj.Category = true;
+        obj.Profile = false;
+      }
+
+      if (iconText === "Profile") {
+        obj.Home = false;
+        obj.Category = false;
+        obj.Profile = true;
+      }
+      setWhoIsClick(obj);
+    };
+
+    return (
+      <div onClick={handleClick}>
+        <IconButton
+          style={{
+            borderBottom: clickFun.whoIsClick[iconText] && "3px solid red",
+          }}
+          className="flex flex-col  "
+          sx={{
+            color: "black",
+            "&:hover": {
+              color: "red",
+            },
+          }}
+        >
+          <Icon />
+          <p className="font-bold font-sans text-sm ">{iconText}</p>
+        </IconButton>
+      </div>
+    );
+  };
 
   const [whoIsClick, setWhoIsClick] = useState({
     Home: true,
@@ -19,9 +55,21 @@ const Footer = () => {
   });
   return (
     <footer className="fixed bottom-0  left-0 z-[100] drop-shadow-header-shadow w-full p-5 bg-header-background flex items-center justify-between py-3 px-2 ">
-      <FooterIcon Icon={HomeOutlinedIcon} iconText="Home" />
-      <FooterIcon Icon={AppsOutlinedIcon} iconText="Category" />
-      <FooterIcon Icon={AccountCircleOutlinedIcon} iconText="Profile" />
+      <FooterIcon
+        clickFun={{ whoIsClick, setWhoIsClick }}
+        Icon={HomeOutlinedIcon}
+        iconText="Home"
+      />
+      <FooterIcon
+        clickFun={{ whoIsClick, setWhoIsClick }}
+        Icon={AppsOutlinedIcon}
+        iconText="Category"
+      />
+      <FooterIcon
+        clickFun={{ whoIsClick, setWhoIsClick }}
+        Icon={AccountCircleOutlinedIcon}
+        iconText="Profile"
+      />
     </footer>
   );
 };
