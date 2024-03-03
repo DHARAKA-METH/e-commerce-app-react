80f51b5d74e2606b7dddcf90d8fa44b187951000
DHARAKA DM <deshandm699@gmail.com>
2024-03-03 14:46:06 +0530

    update

--- src/Pages/Category/Category.js
+++ src/Pages/Category/Category.js
@@ -12,7 +12,7 @@ const Category = () => {
         }}
       >
         <div><h2 className="text-lg font-bold mb-3 ">Main Categories..</h2></div>
-        <div className="w-full grid sm:grid-cols-1 lg:grid-cols-3 grid-rows-[auto] gap-5">
+        <div  className="w-full grid sm:grid-cols-1 lg:grid-cols-3 grid-rows-[auto] gap-5">
           <CategoryItems />
           <CategoryItems />
           <CategoryItems />
@@ -30,11 +30,18 @@ const Category = () => {
 
 export default Category;
 const CategoryItems = () => (
-    <IconButton sx={{color:"black"}}>
+    <IconButton style={{
+      boxShadow: "rgba(0,0,0,0.2)0px 3px 8px",
+      padding: "20px",
+      borderRadius: "25px",
+    }} sx={{color:"black"}}>
   <div className="w-ful p-1  flex flex-col items-center">
     <img 
-    className="rounded-lg object-contain"
+    className="rounded-lg "
       src="https://www.evetech.co.za/repository/ProductImages/asus-g531gw-core-i7-rtx-2070-gaming-laptop-700px-v9.jpg"
+      style={{
+        borderRadius:"50%"
+      }}
       
       alt="Category Item"
     />
