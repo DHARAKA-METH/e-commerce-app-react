c5362fd0aa39af0c17ca0b0477de22bb3b657052
DHARAKA DM <deshandm699@gmail.com>
2024-03-03 13:53:17 +0530

    category design

--- src/Pages/Category/Category.js
+++ src/Pages/Category/Category.js
@@ -1,20 +1,28 @@
+import { IconButton } from "@mui/material";
+
 const Category = () => {
   return (
     <div className="px-5 py-[100px]  w-full h-screen overflow-y-scroll">
       <div
-        className="w-full grid grid-cols-3 grid-rows-[auto] gap-5"
+        className="w-full flex flex-col "
         style={{
           boxShadow: "rgba(0,0,0,0.24)0px 3px 8px",
           padding: "20px",
           borderRadius: "25px",
         }}
       >
-<CategoryItems/>
-<CategoryItems/>
-<CategoryItems/>
-<CategoryItems/>
-<CategoryItems/>
-
+        <div><h2 className="text-lg font-bold mb-3 ">Main Categories..</h2></div>
+        <div className="w-full grid sm:grid-cols-1 lg:grid-cols-3 grid-rows-[auto] gap-5">
+          <CategoryItems />
+          <CategoryItems />
+          <CategoryItems />
+          <CategoryItems />
+          <CategoryItems />
+          <CategoryItems />
+          <CategoryItems />
+          <CategoryItems />
+          <CategoryItems />
+        </div>
       </div>
     </div>
   );
@@ -22,11 +30,16 @@ const Category = () => {
 
 export default Category;
 const CategoryItems = () => (
-  <div className="w-ful p-1  rounded-md  flex flex-col items-center">
-    <img
-      src="https://i.ebayimg.com/images/g/AC0AAOSwXsZkdMXl/s-l1600.jpg"
+    <IconButton sx={{color:"black"}}>
+  <div className="w-ful p-1  flex flex-col items-center">
+    <img 
+    className="rounded-lg object-contain"
+      src="https://www.evetech.co.za/repository/ProductImages/asus-g531gw-core-i7-rtx-2070-gaming-laptop-700px-v9.jpg"
+      
       alt="Category Item"
     />
-    <h2 className="mt-[-20px] p-1  font-semibold" >Category Name</h2>
+    <h2 className="text-[20px] font-bold">Category Name</h2>
+  
   </div>
+    </IconButton>
 );
