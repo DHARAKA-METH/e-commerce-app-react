52fff755240f90eeef3d9b5bc2f105a96442a7f5
DHARAKA DM <deshandm699@gmail.com>
2024-03-03 11:52:01 +0530

    Category design

--- src/Pages/Category/Category.js
+++ src/Pages/Category/Category.js
@@ -1,10 +1,32 @@
-
 const Category = () => {
   return (
     <div className="px-5 py-[100px]  w-full h-screen overflow-y-scroll">
-      Category
-    </div>
-  )
-}
+      <div
+        className="w-full grid grid-cols-3 grid-rows-[auto] gap-5"
+        style={{
+          boxShadow: "rgba(0,0,0,0.24)0px 3px 8px",
+          padding: "20px",
+          borderRadius: "25px",
+        }}
+      >
+<CategoryItems/>
+<CategoryItems/>
+<CategoryItems/>
+<CategoryItems/>
+<CategoryItems/>
 
-export default Category
+      </div>
+    </div>
+  );
+};
+
+export default Category;
+const CategoryItems = () => (
+  <div className="w-ful p-1  rounded-md  flex flex-col items-center">
+    <img
+      src="https://i.ebayimg.com/images/g/AC0AAOSwXsZkdMXl/s-l1600.jpg"
+      alt="Category Item"
+    />
+    <h2 className="mt-[-20px] p-1  font-semibold" >Category Name</h2>
+  </div>
+);
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,21 +1,19 @@
-import { useState } from 'react'
-import Ads from './Ads'
-import Products from './Products'
-import Loading from '../../components/Loading/Loading'
+import { useState } from "react";
+import Ads from "./Ads";
+import Products from "./Products";
+import Loading from "../../components/Loading/Loading";
 
 const Home = () => {
-  const [loading,setLoading] =useState(false)
+  const [loading, setLoading] = useState(false);
 
-  if(loading)return<Loading/>
+  if (loading) return <Loading />;
   return (
     <div className="px-5 py-[100px]  w-full h-screen overflow-y-scroll">
-      <Ads/>
-      <Products title={'Trending Products..'} slidesPerView={2}/>
-      <Products title={'Computer, Office & Education..'} slidesPerView={3}/>
-      <Products title={'Home & Garden..'} slidesPerView={4}/>
-
+      <Ads />
+      <Products title={"Trending Products.."} slidesPerView={2} />
+      <Products title={"Computer, Office & Education.."} slidesPerView={3} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
