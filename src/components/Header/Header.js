d8197dd47dcafeff6de359cc636fcb7bae56ab2e
DHARAKA DM <deshandm699@gmail.com>
2024-03-05 17:02:11 +0530

    stoped re rendering when search box clicked

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,26 @@ import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import { IconButton } from "@mui/material";
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+let clicked = false;
 
 const Header = () => {
-  const [isSearch, setIsSearch] = useState(false);
+  console.log("re rendering");
+  //const [isSearch, setIsSearch] = useState(false);
+
+  const logoRef = useRef();
+  const searchBoxRef = useRef();
+  const searchButtonClickHandle = () => {
+    if (clicked) {
+      logoRef.current.style = "display:none;";
+      searchBoxRef.current.style = "display:inline-block;";
+    } else {
+      logoRef.current.style = "display:inline-block;";
+      searchBoxRef.current.style = "display:none;";
+    }
+  };
+
   return (
     <header className="fixed top-0 left-0 z-[100] drop-shadow-header-shadow w-full p-5 bg-header-background flex items-center justify-between py-2 px-2">
       {/* header left */}
@@ -19,11 +35,12 @@ const Header = () => {
           />
         </IconButton>
         <h1
+          ref={logoRef}
           className="ml-2 text-sm font-bold"
           style={{
             display:
               window.innerWidth < 640
-                ? isSearch
+                ? clicked
                   ? "none"
                   : "inline-block"
                 : "inline-block",
@@ -33,12 +50,13 @@ const Header = () => {
         </h1>
         <div className="overflow-hidden ml-2 flex items-center rounded-full bg-[#fdf3f3]  ">
           <input
+            ref={searchBoxRef}
             type="text"
             placeholder="Search"
             style={{
               display:
                 window.innerWidth < 640
-                  ? isSearch
+                  ? clicked
                     ? "inline-block"
                     : "none"
                   : "inline-block",
@@ -48,7 +66,10 @@ const Header = () => {
           <IconButton
             onClick={() => {
               if (window.innerWidth < 640) {
-                setIsSearch(!isSearch);
+                //   setIsSearch(!isSearch);
+                clicked = !clicked;
+                searchButtonClickHandle();
+                console.log(clicked);
               }
             }}
           >
