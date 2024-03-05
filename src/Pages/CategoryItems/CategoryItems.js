3344715e54b38e293b37d0b372b62d0846546da0
DHARAKA DM <deshandm699@gmail.com>
2024-03-05 12:10:45 +0530

    add options

--- src/Pages/CategoryItems/CategoryItems.js
+++ src/Pages/CategoryItems/CategoryItems.js
@@ -1,39 +1,28 @@
-import { IconButton, Rating } from "@mui/material";
+import { IconButton, Rating, colors } from "@mui/material";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 // import { CategorySelector } from "../../Store/ReduxSlice/CategorySlice"
 
-
 const categoryItemArr = [
   {
-    imgUrl:
-      "https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg",
+    imgUrl: "https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg",
     title: "Category Title1",
-    
   },
   {
-    imgUrl:
-      "https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg",
+    imgUrl: "https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg",
     title: "Category Title2",
-    
   },
   {
-    imgUrl:
-      "https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg",
+    imgUrl: "https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg",
     title: "Category Title3",
-    
   },
   {
-    imgUrl:
-      "https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg",
+    imgUrl: "https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg",
     title: "Category Title4",
-    
   },
   {
-    imgUrl:
-      "https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg",
+    imgUrl: "https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg",
     title: "Category Title5",
-    
   },
 ];
 
@@ -49,7 +38,9 @@ const CategoryItems = () => {
         {CategoryTitle.title}
       </h1>
       <div className="w-full grid grid-cols-2 grid-rows-[auto] gap-3">
-        {categoryItemArr.map(({imgUrl,title},index)=><CategoryItemArrUnit key={index} imageurl={imgUrl} title={title}/>)}
+        {categoryItemArr.map(({ imgUrl, title }, index) => (
+          <CategoryItemArrUnit key={index} imageurl={imgUrl} title={title} />
+        ))}
       </div>
     </div>
   );
@@ -57,12 +48,21 @@ const CategoryItems = () => {
 
 export default CategoryItems;
 
-const CategoryItemArrUnit =({imageurl,title})=>
-<div>
-<IconButton>  
-  <img src={imageurl} alt={title} className="w-full object-contain"/>
-</IconButton>
-  <h3>{title}</h3>
-  <h5 className="font-bold">LKR 1000</h5>
-  <Rating name="size-small" defaultValue={3} size="small" readOnly />
-</div>
+const CategoryItemArrUnit = ({ imageurl, title }) => (
+  <IconButton
+    sx={{
+      color: "gray",
+      padding: "5px",
+      "&:hover": {
+        color: "gray",
+      },
+    }}
+  >
+    <div style={{ boxShadow: "rgba(0,0,0,0.24)0px 3px 8px", padding: "10px" }}>
+      <img src={imageurl} alt={title} className="w-full object-contain" />
+      <h3>{title}</h3>
+      <h5 className="font-bold">LKR 1000</h5>
+      <Rating name="size-small" defaultValue={3} size="small" readOnly />
+    </div>
+  </IconButton>
+);
