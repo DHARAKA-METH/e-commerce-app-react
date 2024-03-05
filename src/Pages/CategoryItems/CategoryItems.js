d9d9751c510d6eb9962e26bcf08c8bf124e5ab1f
DHARAKA DM <deshandm699@gmail.com>
2024-03-05 11:42:52 +0530

    category update

--- src/Pages/CategoryItems/CategoryItems.js
+++ src/Pages/CategoryItems/CategoryItems.js
@@ -1,14 +1,20 @@
 import { useSelector } from "react-redux"
+import { useParams } from "react-router-dom"
 // import { CategorySelector } from "../../Store/ReduxSlice/CategorySlice"
 
 
 
 const CategoryItems = () => {
+  const {CategoryId}=useParams();
   const category =useSelector(store=>store.category)
+  const [CategoryTitle]=category.filter(ele=>ele.id===CategoryId)
+  console.log('Category title',CategoryTitle)
   console.log(category)
   return (
     <div className="px-5 py-[100px]  w-full h-screen overflow-y-scroll">
-      CategoryItems
+      <h1 className="ml-3 font-bold text-lg mt-2 mb-3 ">
+      {CategoryTitle.title}
+      </h1>
     </div>
   )
 }
