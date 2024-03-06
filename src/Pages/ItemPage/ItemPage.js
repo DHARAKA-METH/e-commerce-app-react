e49035ef080faf15746e9a7f61854feb99365ce4
DHARAKA DM <deshandm699@gmail.com>
2024-03-06 12:13:01 +0530

    item page design

--- src/Pages/ItemPage/ItemPage.js
+++ src/Pages/ItemPage/ItemPage.js
@@ -1,7 +1,19 @@
+import { useRef } from "react"
+
+const itemImageArr=[
+  'https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg',
+  'https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg',
+  'https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg',
+  'https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg',
+]
+
 const ItemPage = () => {
+  const mainImageRef=useRef()
+  const subImageRef=useRef([])
   return (
     <div className="px-5 py-[100px]  w-full h-screen overflow-y-scroll">
-      <h1 className="text-lg font-bold">Item Title</h1>
+      <h1 className="text-lg font-bold px-3 mb-7">Item Title</h1>
+      <img src="" alt="" />
     </div>
   )
 }
