6fa88f952543bfb8582048eb3d6e932b088e6479
DHARAKA DM <deshandm699@gmail.com>
2024-03-07 14:56:38 +0530

    configer model in home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,24 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Ads from "./Ads";
 import Products from "./Products";
 import Loading from "../../components/Loading/Loading";
+import PyamentModel from "../../Models/PyamentModel";
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
+  const paymentModelRef = useRef();
 
   if (loading) return <Loading />;
   return (
     <div className=" px-5 py-[100px] sm:px-16 md:px-[120px] lg:px-[200px] w-full h-screen overflow-y-scroll">
+      <PyamentModel ref={paymentModelRef} />
+      <button
+        onClick={() => {
+          paymentModelRef.current.handleOpen();
+        }}
+      >
+        Click
+      </button>
       <Ads />
       <Products title={"Trending Products.."} slidesPerView={2} />
       <Products title={"Computer, Office & Education.."} slidesPerView={3} />
