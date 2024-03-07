3bc632a591f9d10e4dc465f25c6642f8e526c72f
DHARAKA DM <deshandm699@gmail.com>
2024-03-07 09:39:08 +0530

    add media query sm md lg

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
 
   if (loading) return <Loading />;
   return (
-    <div className="px-5 py-[100px]  w-full h-screen overflow-y-scroll">
+    <div className="px-5 py-[100px] sm:px-16 md:px-[120px] lg:px-[200px] w-full h-screen overflow-y-scroll">
       <Ads />
       <Products title={"Trending Products.."} slidesPerView={2} />
       <Products title={"Computer, Office & Education.."} slidesPerView={3} />
