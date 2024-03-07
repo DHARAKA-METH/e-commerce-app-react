d77850992844e91a1f7b1000ed94ba12beb66cdb
DHARAKA DM <deshandm699@gmail.com>
2024-03-07 14:43:11 +0530

    add model form metirial ui

---
+++ src/Models/PyamentModel.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Modal from '@mui/material/Modal';
+
+
+
+export default function PyamentModel() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+     
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+
+      </Modal>
+    </div>
+  );
+}
