869119c99dbbc12c6c240a08b16cf5090956a14e
DHARAKA DM <deshandm699@gmail.com>
2024-03-09 13:34:52 +0530

    stop when render the componennts ,fetch data mulpiple time form database

--- src/Utils/DataFetch/GetDataFromCollection.js
+++ src/Utils/DataFetch/GetDataFromCollection.js
@@ -1,21 +1,28 @@
 import { collection, getDocs } from "firebase/firestore";
 import db from "../../Firebase/Firebase";
+const dbData = {};
 
 const GetDataFromCollection = (collectionName, setFunction) => {
-  getDocs(collection(db, collectionName))
-    .then((querySnapshot) => {
-      const dataArr = [];
-      querySnapshot.forEach((doc) => {
-        dataArr.push({ ...doc.data(), categoryId: doc.id });
-        //console.log(doc.id, " => ", doc.data());
-        // doc.data() is never undefined for query doc snapshots
+  if (dbData[collectionName] && dbData[collectionName].length > 0) {
+    setFunction(dbData[collectionName]);
+  } else {
+    getDocs(collection(db, collectionName))
+      .then((querySnapshot) => {
+        const dataArr = [];
+        querySnapshot.forEach((doc) => {
+          dataArr.push({ ...doc.data(), categoryId: doc.id });
+          //console.log(doc.id, " => ", doc.data());
+          // doc.data() is never undefined for query doc snapshots
+        });
+        //console.log("db data reading.....");
+        //console.log(dataArr);
+        setFunction(dataArr);
+        dbData[collectionName] = dataArr;
+      })
+      .catch((error) => {
+        // console.log("Error getting documents: ", error);
       });
-      console.log(dataArr);
-      setFunction(dataArr);
-    })
-    .catch((error) => {
-      // console.log("Error getting documents: ", error);
-    });
+  }
 };
 
 export default GetDataFromCollection;
