8b2d067b16d59f631af22e3c6ff252ab8c5e87b0
DHARAKA DM <deshandm699@gmail.com>
2024-03-09 13:26:47 +0530

    updates

--- src/Utils/DataFetch/GetDataFromCollection.js
+++ src/Utils/DataFetch/GetDataFromCollection.js
@@ -1,21 +1,20 @@
 import { collection, getDocs } from "firebase/firestore";
 import db from "../../Firebase/Firebase";
 
-const GetDataFromCollection = (collectionName,setFunction) => {
+const GetDataFromCollection = (collectionName, setFunction) => {
   getDocs(collection(db, collectionName))
     .then((querySnapshot) => {
-        const dataArr=[]
-        querySnapshot.forEach((doc) => {
-            dataArr.push({...doc.data(),categoryId:doc.id})
-            //console.log(doc.id, " => ", doc.data());
+      const dataArr = [];
+      querySnapshot.forEach((doc) => {
+        dataArr.push({ ...doc.data(), categoryId: doc.id });
+        //console.log(doc.id, " => ", doc.data());
         // doc.data() is never undefined for query doc snapshots
-        
-    });
-    console.log(dataArr)
-    setFunction(dataArr)
-})
+      });
+      console.log(dataArr);
+      setFunction(dataArr);
+    })
     .catch((error) => {
-      console.log("Error getting documents: ", error);
+      // console.log("Error getting documents: ", error);
     });
 };
 
