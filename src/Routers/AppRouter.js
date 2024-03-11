d16a46cd431900b596a95aefa4a48ba5191c8f17
DHARAKA DM <deshandm699@gmail.com>
2024-03-11 10:14:16 +0530

    routing update to object routing method

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -15,57 +15,79 @@ import Category from "../Pages/Category/Category";
 import CategoryItems from "../Pages/CategoryItems/CategoryItems";
 import ItemPage from "../Pages/ItemPage/ItemPage";
 
-// const router=createBrowserRouter([
-//   {
-//     path:'/',
-//     element:<MainLayout/>,
-//     children:[
-//       {
-//         index:true,
-//         element:<Home/>
-//       },
-//       {
-//         element:<UserProtected/>,
-//         children:[
-//           {
-//             path:'user',
-//             element:<User/>
-//           }
-//         ]
-//       }
 
-//     ]
-
-// },
-// {
-//   path:'login',
-//   element:<Login/>
-// }
-// ])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+        
+      },
+      {
+        path: "category",
+        children: [
+          {
+            index: true,
+            element: <Category />,
+          },
+          {
+            path: ":CategoryId",
+            children: [
+              {
+                index: true,
+                element: <CategoryItems />,
+              },
+              {
+                path: "item",
+                element: <ItemPage />,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        element: <UserProtected />,
+        children: [
+          {
+            path: "user",
+            element: <User />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "/profile",
+    element: <Login />,
+  },
+]);
 
 const AppRouter = () => {
   return (
-    // <RouterProvider router={router}/>
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route path="category" >
-              <Route index element={<Category />}/>
-              <Route path=":CategoryId"  >
-                <Route index element={<CategoryItems />}/>
-                <Route path="item" element={<ItemPage/>}/>
-              </Route>
-            </Route>
-            <Route element={<UserProtected />}>
-              <Route path="user" element={<User />} />
-            </Route>
-          </Route>
-          <Route path="/profile" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <RouterProvider router={router} />
+    // <div>
+    //   <BrowserRouter>
+    //     <Routes>
+    //       <Route path="/" element={<MainLayout />}>
+    //         <Route index element={<Home />} />
+    //         <Route path="category">
+    //           <Route index element={<Category />} />
+    //           <Route path=":CategoryId">
+    //             <Route index element={<CategoryItems />} />
+    //             <Route path="item" element={<ItemPage />} />
+    //           </Route>
+    //         </Route>
+    //         <Route element={<UserProtected />}>
+    //           <Route path="user" element={<User />} />
+    //         </Route>
+    //       </Route>
+    //       <Route path="/profile" element={<Login />} />
+    //     </Routes>
+    //   </BrowserRouter>
+    // </div>
   );
 };
 
