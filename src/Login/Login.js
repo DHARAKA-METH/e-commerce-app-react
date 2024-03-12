7c3c17ed5f3d90538608b982ff2bb9409d323559
DHARAKA DM <deshandm699@gmail.com>
2024-03-12 12:29:01 +0530

    sign in with firebase authontication

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,9 +1,10 @@
 import { useRef } from "react";
 import { useDispatch } from "react-redux";
 import { collection, query, where, getDocs } from "firebase/firestore";
-import db from "../Firebase/Firebase";
+import db, { auth } from "../Firebase/Firebase";
 import { addUser } from "../Store/ReduxSlice/UserSlice";
 import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 
 const Login = () => {
@@ -11,26 +12,46 @@ const Login = () => {
   const dispatch = useDispatch();
 
   const loginSubmit = () => {
-    const q = query(collection(db, "users"), where("username", "==", "admin"));
+ 
+    const email=EmailRef.current.value
+    const password=passwordRef.current.value
 
-    getDocs(q).then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-        dispatch(addUser(doc.data()))
-        navigate("/admin")
+
+
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user)
+    // ...
+  })
+  .catch((error) => {
+    //const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage)
+  });
+
+
+    // const q = query(collection(db, "users"), where("username", "==", "admin"));
+
+    // getDocs(q).then((querySnapshot) => {
+    //   querySnapshot.forEach((doc) => {
+    //     // doc.data() is never undefined for query doc snapshots
+    //     console.log(doc.id, " => ", doc.data());
+    //     dispatch(addUser(doc.data()))
+    //     navigate("/admin")
         
-      });
-    });
+    //   });
+    // });
   };
-  const usernameRef = useRef();
+  const EmailRef = useRef();
   const passwordRef = useRef();
   return (
     <div className="mt-5 flex flex-col items-center space-y-4">
       <input
-        ref={usernameRef}
+        ref={EmailRef}
         type="text"
-        placeholder="Username"
+        placeholder="Email"
         className="border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500"
       />
       <input
