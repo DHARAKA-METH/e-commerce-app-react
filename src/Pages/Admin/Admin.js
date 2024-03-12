6793a6b273082bc6af6308fb298067ce1627f406
DHARAKA DM <deshandm699@gmail.com>
2024-03-12 11:05:34 +0530

    logout button set remove user dispatch

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { removeUser } from '../../Store/ReduxSlice/UserSlice'
 
 const Admin = () => {
+  const dispatch=useDispatch()
   return (
     <div className="px-5 py-[100px]  w-full h-screen overflow-y-scroll">
       <h1>admin</h1>
+      <button onClick={()=>dispatch(removeUser())}>LogOut</button>
     </div>
   )
 }
