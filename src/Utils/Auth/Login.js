19cc1facd8e1d706bac46bf4411773449f058b94
DHARAKA DM <deshandm699@gmail.com>
2024-03-14 12:58:40 +0530

    login.js

---
+++ src/Utils/Auth/Login.js
@@ -0,0 +1,21 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../Firebase/Firebase";
+
+const Login = (email, password) => {
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user)
+      // ...
+    })
+    .catch((error) => {
+     // const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage)
+    });
+
+}
+
+export default Login
