b75a0f3c596a86be5e6a9d65127bf2ea02d6893e
DHARAKA DM <deshandm699@gmail.com>
2024-03-14 16:17:39 +0530

    add functions

--- src/Utils/Auth/Register.js
+++ src/Utils/Auth/Register.js
@@ -1,20 +1,36 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../Firebase/Firebase";
+import SetDataDocument from "../DataFetch/SetDataDocument";
 
-const Register = (email,password) => {
-    createUserWithEmailAndPassword(auth, email, password)
+const Register = (
+  email,
+  password,
+  name,
+  address,
+  phoneNumber,
+  profileImage
+) => {
+  createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Signed up 
+      // Signed up
       const user = userCredential.user;
-      console.log(user)
+      //console.log(user)
+      const userDataSet = {
+        name,
+        address,
+        phoneNumber,
+        profileImage,
+        uid: user.uid,
+      };
+      SetDataDocument("users", user.uid, userDataSet);
       // ...
     })
     .catch((error) => {
       //const errorCode = error.code;
       const errorMessage = error.message;
-      console.log(errorMessage)
+      console.log(errorMessage);
       // ..
     });
-}
+};
 
-export default Register
+export default Register;
