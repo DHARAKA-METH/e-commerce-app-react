40939635c30bfe2fff2dd9e2f463f85f2568c6a9
DHARAKA DM <deshandm699@gmail.com>
2024-03-15 11:07:29 +0530

    isRegisterdUser add to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppRouter from "./Routers/AppRouter";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./Firebase/Firebase";
 
 const App = () => {
+  useEffect(() => {
+    const isRegisterdUser = () => {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/auth.user
+          const uid = user.uid;
+          console.log("user ID-", uid);
+          // ...
+        } else {
+          console.log("User is signed out or not registered");
+          // ...
+        }
+      });
+    };
+    //isRegisterdUser();
+    return () => isRegisterdUser();
+  }, []);
+
   return <AppRouter />;
 };
 
