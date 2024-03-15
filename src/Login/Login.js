e3e989943d7c6f5b719f25e4a1f3a90bc9ee1191
DHARAKA DM <deshandm699@gmail.com>
2024-03-15 11:02:44 +0530

    update

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -13,10 +13,10 @@ const Login = () => {
           // User is signed in, see docs for a list of available properties
           // https://firebase.google.com/docs/reference/js/auth.user
           const uid = user.uid;
-          console.log('user ID-',uid)
+          console.log("user ID-", uid);
           // ...
         } else {
-          console.log('User is signed out or not registered') 
+          console.log("User is signed out or not registered");
           // ...
         }
       });
@@ -116,114 +116,119 @@ const Login = () => {
           <h2 className="text-2xl font-semibold mb-4">Register</h2>
 
           <form onSubmit={registerHandle}>
-            {/* Name Input */}
-            <div className="mb-4">
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
+            <div className="flex flex-row gap-4">
+              <div>
+                {/* Name Input */}
+                <div className="mb-4">
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="mt-1 p-2 w-full border rounded-md"
+                  />
+                </div>
 
-            {/* Email Input */}
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
+                {/* Email Input */}
+                <div className="mb-4">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="mt-1 p-2 w-full border rounded-md"
+                  />
+                </div>
 
-            {/* user address Input */}
-            <div className="mb-4">
-              <label
-                htmlFor="address"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Address
-              </label>
-              <input
-                type="text"
-                id="address"
-                name="address"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
-            {/* MobileNumber Input */}
-            <div className="mb-4">
-              <label
-                htmlFor="mobile"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Mobile Number
-              </label>
-              <input
-                type="number"
-                id="mobile"
-                name="mobile"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
-            {/* Profile Image Input */}
-            <div className="mb-4">
-              <label
-                htmlFor="profile"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Profile Image
-              </label>
-              <input
-                type="text"
-                id="profile"
-                name="profile"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
+                {/* user address Input */}
+                <div className="mb-4">
+                  <label
+                    htmlFor="address"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Address
+                  </label>
+                  <input
+                    type="text"
+                    id="address"
+                    name="address"
+                    className="mt-1 p-2 w-full border rounded-md"
+                  />
+                </div>
+                {/* MobileNumber Input */}
+                <div className="mb-4">
+                  <label
+                    htmlFor="mobile"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Mobile Number
+                  </label>
+                  <input
+                    type="number"
+                    id="mobile"
+                    name="mobile"
+                    className="mt-1 p-2 w-full border rounded-md"
+                  />
+                </div>
+                {/* Profile Image Input */}
+                <div className="mb-4">
+                  <label
+                    htmlFor="profile"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Profile Image
+                  </label>
+                  <input
+                    type="text"
+                    id="profile"
+                    name="profile"
+                    className="mt-1 p-2 w-full border rounded-md"
+                  />
+                </div>
+              </div>
+              <div>
+                {/* Password Input */}
+                <div className="mb-6">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="mt-1 p-2 w-full border rounded-md"
+                  />
+                </div>
 
-            {/* Password Input */}
-            <div className="mb-6">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
-            </div>
-
-            {/* re-Password Input */}
-            <div className="mb-6">
-              <label
-                htmlFor="repassword"
-                className="block text-sm font-medium text-gray-600"
-              >
-                Re-Password
-              </label>
-              <input
-                type="password"
-                id="repassword"
-                name="repassword"
-                className="mt-1 p-2 w-full border rounded-md"
-              />
+                {/* re-Password Input */}
+                <div className="mb-6">
+                  <label
+                    htmlFor="repassword"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Re-Password
+                  </label>
+                  <input
+                    type="password"
+                    id="repassword"
+                    name="repassword"
+                    className="mt-1 p-2 w-full border rounded-md"
+                  />
+                </div>
+              </div>
             </div>
 
             {/* Login Button */}
