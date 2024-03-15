52b0b6d7817ac502fba8318171eb223a24b3a66d
DHARAKA DM <deshandm699@gmail.com>
2024-03-15 12:07:10 +0530

    isRegisterdUser or loginUser add redux store and is logout remove user from redux store

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React, { useEffect } from "react";
 import AppRouter from "./Routers/AppRouter";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./Firebase/Firebase";
+import getDataFromDocument from "./Utils/DataFetch/getDataFromDocument";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, removeUser } from "./Store/ReduxSlice/UserSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const userData = useSelector((store) => store.UserSlice.user);
+  console.log("userDataOnStore...", userData);
   useEffect(() => {
     const isRegisterdUser = () => {
       onAuthStateChanged(auth, (user) => {
@@ -12,9 +18,13 @@ const App = () => {
           // https://firebase.google.com/docs/reference/js/auth.user
           const uid = user.uid;
           console.log("user ID-", uid);
+          getDataFromDocument("users", uid, (dataSet) => {
+            dispatch(addUser(dataSet));
+          });
           // ...
         } else {
           console.log("User is signed out or not registered");
+          dispatch(removeUser());
           // ...
         }
       });
