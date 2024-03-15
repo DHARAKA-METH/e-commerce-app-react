498227590cb56e0e01d9ae763249c8666b4fedfd
DHARAKA DM <deshandm699@gmail.com>
2024-03-15 19:21:34 +0530

    make mistake in admin protector

--- src/Routers/AdminProtected.js
+++ src/Routers/AdminProtected.js
@@ -1,23 +1,21 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import {  Outlet, useNavigate } from "react-router-dom";
 //import { userSelector } from "../Store/ReduxSlice/UserSlice"
 
 const AdminProtected = () => {
   const navigate = useNavigate();
 
   const userData = useSelector((store) => store.UserSlice.user);
-  //console.log('user in admin protector.....',user);
+ // console.log('user in admin protector.....',userData.role);
 
   useEffect(() => {
     if (userData.name) {
-      if (!userData.name === "default") {
-        if (!userData.role === "admin") {
+      if (!(userData.name === "default")) {
+        if (!(userData.role === "admin")) {
           navigate("/4O4");
         }
-      } else {
-        navigate("/4O4");
-      }
+      } 
     }
     // eslint-disable-next-line
   }, [userData]);
