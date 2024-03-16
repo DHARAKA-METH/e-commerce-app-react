c4457d2e98b8188e412c269387f62d8609f1dd3c
DHARAKA DM <deshandm699@gmail.com>
2024-03-16 18:21:30 +0530

    update

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { removeUser } from '../../Store/ReduxSlice/UserSlice'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { removeUser } from "../../Store/ReduxSlice/UserSlice";
 
 const Admin = () => {
-  const dispatch=useDispatch()
+  const dispatch = useDispatch();
   return (
     <div className="px-5 py-[100px]  w-full h-screen overflow-y-scroll">
       <h1>admin</h1>
-      <button onClick={()=>dispatch(removeUser())}>LogOut</button>
+      <button onClick={() => dispatch(removeUser())}>LogOut</button>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
