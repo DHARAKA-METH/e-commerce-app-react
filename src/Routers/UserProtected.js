c7c59dfbc291fabef51a4a720f3eaf556bc2f3f6
DHARAKA DM <deshandm699@gmail.com>
2024-03-16 18:22:25 +0530

    update

--- src/Routers/UserProtected.js
+++ src/Routers/UserProtected.js
@@ -2,10 +2,9 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
-
 const UserProtected = () => {
   const userData = useSelector((store) => store.UserSlice.user);
- // const user = false;
+  // const user = false;
 
   return userData.name ? <Outlet /> : <Navigate to="/login" />;
 };
