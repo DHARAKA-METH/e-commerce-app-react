fff1bb1455fc3a8158849debefb1facc6fe25fad
DHARAKA DM <deshandm699@gmail.com>
2024-03-16 18:21:56 +0530

    update

--- src/Pages/User/User.js
+++ src/Pages/User/User.js
@@ -1,19 +1,18 @@
-import logoutUser from "../../Utils/Auth/logoutUser"
+import logoutUser from "../../Utils/Auth/logoutUser";
 
 const User = () => {
   return (
     <div className="pt-[200px]">
       User
-
       <button
-            onClick={() => logoutUser()}
-            className="bg-blue-500 hover:bg-blue-700 text-white p-2 mt-2 rounded w-full"
-            type="submit"
-          >
-            Log Out
-          </button>
+        onClick={() => logoutUser()}
+        className="bg-blue-500 hover:bg-blue-700 text-white p-2 mt-2 rounded w-full"
+        type="submit"
+      >
+        Log Out
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
