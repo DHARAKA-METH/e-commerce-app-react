3a5eea6546c041dd6edf0731138a1a25f00427d8
DHARAKA DM <deshandm699@gmail.com>
2024-03-17 20:23:30 +0530

    update

--- src/Routers/AdminProtected.js
+++ src/Routers/AdminProtected.js
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import {  Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 //import { userSelector } from "../Store/ReduxSlice/UserSlice"
 
 const AdminProtected = () => {
   const navigate = useNavigate();
 
   const userData = useSelector((store) => store.UserSlice.user);
- // console.log('user in admin protector.....',userData.role);
+  // console.log('user in admin protector.....',userData.role);
 
   useEffect(() => {
     if (userData.name) {
@@ -15,9 +15,9 @@ const AdminProtected = () => {
         if (!(userData.role === "admin")) {
           navigate("/4O4");
         }
-      } else{
-        navigate("/4O4");
       }
+    } else {
+      navigate("/4O4");
     }
     // eslint-disable-next-line
   }, [userData]);
