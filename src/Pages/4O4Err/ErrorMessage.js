be3cf2dbe233761657c64a2f3ba8c40f77a144d0
DHARAKA DM <deshandm699@gmail.com>
2024-03-20 08:10:26 +0530

    4O4 error page design

--- src/Pages/4O4Err/ErrorMessage.js
+++ src/Pages/4O4Err/ErrorMessage.js
@@ -1,7 +1,15 @@
 const ErrorMessage = () => {
   return (
-    <div className="w-full  flex flex-col justify-center items-center">
-      <h1 className="font-bold text-8xl">4O4 Erorr...</h1>    
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="container">
+        <div className="text-center">
+          <h1 className="text-6xl font-semibold text-gray-800">404</h1>
+          <p className="text-xl font-medium text-gray-500">We can't find the page you're looking for.</p>
+          <button onClick={() => window.history.back()} className="mt-6 px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-700 transition ease-in-out duration-150">
+            Go back
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
