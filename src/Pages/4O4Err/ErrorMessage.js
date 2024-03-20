7490629b6c0c18e7f90f4a1d9c34d588fdce548a
DHARAKA DM <deshandm699@gmail.com>
2024-03-20 11:14:13 +0530

    set path for error message page

--- src/Pages/4O4Err/ErrorMessage.js
+++ src/Pages/4O4Err/ErrorMessage.js
@@ -1,17 +1,28 @@
+import { useLocation } from "react-router-dom";
+
 const ErrorMessage = () => {
+  const { pathname } = useLocation(); //destructerd useLoaction function
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="container">
         <div className="text-center">
-          <h1 className="text-6xl font-semibold text-gray-800">404</h1>
-          <p className="text-xl font-medium text-gray-500">We can't find the page you're looking for.</p>
-          <button onClick={() => window.history.back()} className="mt-6 px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-700 transition ease-in-out duration-150">
+          <h3 className="text-xl font-semibold text-gray-800">
+            404. That’s an error.
+          </h3>
+          <p className="text-xl font-medium text-gray-500">
+            The requested URL <span className="font-bold">{pathname}</span> was
+            not found on this server. That’s all we know.
+          </p>
+          <button
+            onClick={() => window.history.back()}
+            className="mt-6 px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-700 transition ease-in-out duration-150"
+          >
             Go back
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorMessage
+export default ErrorMessage;
