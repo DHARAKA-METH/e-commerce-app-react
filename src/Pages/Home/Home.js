b39020eb63f2f23b97ca6db4a5e823eb56cf6bea
DHARAKA DM <deshandm699@gmail.com>
2024-03-30 11:57:49 +0530

    nav bar list

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Ads from "./Ads";
 import Products from "./Products";
 import Loading from "../../components/Loading/Loading";
+import GetDataFromCollection from "../../Utils/DataFetch/GetDataFromCollection";
 
 const Home = () => {
-  const [loading, setLoading] = useState(false);
+  const[collectionCategoryData,setCollectionCategoryData]=useState([])
+  useEffect((()=>{
+    GetDataFromCollection('Category',setCollectionCategoryData)
+  }),[])
+  const [loading, setLoading] = useState(true);
+  if (loading) {
+    setTimeout(() => {
+      setLoading(false)
+    }, 100);
+    return<Loading />
 
-  if (loading) return <Loading />;
+} 
   return (
     <div className=" px-5 py-[100px] sm:px-16 md:px-[120px] lg:px-[200px] w-full h-screen overflow-y-scroll">
       <Ads />
--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -1,23 +1,26 @@
+import { useEffect, useState } from "react"
+import GetDataFromCollection from "../../Utils/DataFetch/GetDataFromCollection"
 
 const NavBar = () => {
+
+    const [categoryData,setCategoryData]=useState([])
+    useEffect((()=>{
+      GetDataFromCollection('Category',setCategoryData)
+    }),[])
+
+
   return (
-   <div className="w-[300px] h-[500px] bg-white " style={{
+   <div className="w-[300px] h-[500px] bg-gray-200 " style={{
     
-    borderRadius:'25px',
+borderRadius:'25px',
  position: "absolute",
  marginTop:'25px'
  
    }}>
-   <div className="flex flex-col ml-[30px] mt-[20px] gap-4 ">
-    <h3 className="font-mono text-lg hover:font-semibold cursor-pointer">phones telecommunications</h3>
-    <h3>phones telecommunications</h3>
-    <h3>phones telecommunications</h3>
-    <h3>phones telecommunications</h3>
-    <h3>phones telecommunications</h3>
-    <h3>phones telecommunications</h3>
-    <h3>phones telecommunications</h3>
-    <h3>phones telecommunications</h3>
-    <h3>phones telecommunications</h3>
+   <div className="flex flex-col ml-[30px] mt-[30px] p-2 gap-6 ">
+   {categoryData.map((ele,index)=><h3 key={index} className="font-mono text-lg hover:font-bold cursor-pointer">{ele.CategoryTitle}</h3>)}
+    
+
    </div>
 
     </div>
