8671ab665e6d1b7c180bec033815fffe40d2f99e
DHARAKA DM <deshandm699@gmail.com>
2024-04-01 10:11:32 +0530

    subImageto main image

--- src/Pages/ItemPage/ItemPage.js
+++ src/Pages/ItemPage/ItemPage.js
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import getDataFromDocument from "../../Utils/DataFetch/getDataFromDocument";
 const ItemPage = () => {
 
+  const MainImageRef = useRef()
+  const subImageRef = useRef()
   const currentLocation = useParams()
   const [data, setData] = useState([])
   const imgArr = []
@@ -12,21 +14,26 @@ const ItemPage = () => {
     getDataFromDocument(`Category/${currentLocation.CategoryId}/${currentLocation.CategoryId}`, currentLocation.itemId, setData)
   }, [])
 
+  const imageHandleClick = (src) => {
+MainImageRef.current.src=src
+  }
+
 
   return (
     <div className="w-full h-screen overflow-y-scroll">
       <div className="flex flex-col mt-[100px] ml-[200px] ">
         <div className="w-[500px] h-[500px] object-contain rounded-xl cursor-pointer overflow-hidden">
-          <img  className="transition-transform hover:scale-125" src={data.img2} alt="" />
+          <img ref={MainImageRef} className="transition-transform hover:scale-125" src={data.img2} alt="" />
         </div>
 
         <div className="flex mt-6 ">
-          wire here
+          {imgArr?.map((ele, index) => <div key={index} onClick={()=>imageHandleClick(ele)} className="w-[100px] h-[100px]"><img ref={subImageRef} src={ele} alt="" /></div>)}
         </div>
       </div>
     </div>
 
   );
+
 };
 
 export default ItemPage;
