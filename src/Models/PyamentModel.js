6f40dfe057aa8b8297f159b3e1491a257cce7efb
DHARAKA DM <deshandm699@gmail.com>
2024-04-01 21:16:57 +0530

    design shopping cart

--- src/Models/PyamentModel.js
+++ src/Models/PyamentModel.js
@@ -16,7 +16,7 @@ const PyamentModel = (props, ref) => {
     setDoc(doc(db, "Category/Category4/Category4", " Category4_item1 "), {
       title: "Category4_item1",
       img: "https://m.media-amazon.com/images/I/71nscszW68L._AC_SL1001_.jpg",
-      Rating :"3.5"
+      Rating: "3.5",
     })
       .then(() => {
         console.log("Document written with ID: ");
@@ -25,16 +25,48 @@ const PyamentModel = (props, ref) => {
         console.error("Error adding document: ", error);
       });
   };
+  const ItemUnit = ({imgLink,unitPrice,Qty,Total}) => {
+    return (
+      <div className="grid grid-cols-4  grid-rows-[auto]">
+        <div className="w-[50px] h-[50px] overflow-hidden">
+          {" "}
+          <img src={imgLink} alt="" />
+        </div>
+        <div>100</div>
+        <div>10</div>
+        <div> 10</div>
+      </div>
+    );
+  };
 
   return (
     <Modal
-      className="flex flex-col items-center justify-center"
+      className="flex justify-center items-center overflow-y-scroll"
       open={open}
       onClose={handleClose}
     >
-      <div className="w-[90%] bg-white">
-        Dharaka
-        <button onClick={addData}>..click</button>
+      <div className=" w-[95%] h-[auto] p-5 bg-gray-200 flex overflow-y-scroll  ">
+        <div className="w-3/4">
+          <div className="grid grid-cols-4 text-sm font-semibold grid-rows-[auto]">
+            <div>Product</div>
+            <div>Price</div>
+            <div>Qty</div>
+            <div>Total</div>
+          </div>
+          <div className="mt-1"> <ItemUnit imgLink={'https://rb.gy/302mre'} /></div>
+
+        </div>
+        <div className="w-1/4">
+        <div className="w-full h-[auto] p-5 bg-white">
+         <h1 className="font-bold ml-2">Summary</h1>
+         <div className="flex justify-between"><h3>Discount :</h3><h3>00.00</h3></div>
+         <div className="flex justify-between"><h3>Tax :</h3><h3>00.00</h3></div> 
+         <hr className="border border-black"/>
+         <div className="flex justify-between"><h3>Balance :</h3><h3>1000.00</h3></div>
+         <div className=" mt-2 w-full h-[50px] flex justify-center items-center rounded-3xl bg-blue-600 text-white text-lg font-semibold border border-black hover:bg-black cursor-pointer "><button>CheckOut</button></div>
+        </div>
+          <button onClick={addData}>..click</button>
+        </div>
       </div>
     </Modal>
   );
--- src/Pages/User/User.js
+++ src/Pages/User/User.js
@@ -2,15 +2,27 @@ import logoutUser from "../../Utils/Auth/logoutUser";
 
 const User = () => {
   return (
-    <div className="pt-[200px]">
-      User
-      <button
-        onClick={() => logoutUser()}
-        className="bg-blue-500 hover:bg-blue-700 text-white p-2 mt-2 rounded w-full"
-        type="submit"
-      >
-        Log Out
-      </button>
+    <div className="w-full overflow-y-scroll pt-[200px]">
+      <div className=" flex flex-col items-center ">
+        <div className="w-[200px] h-[200px] rounded-[50%]">
+          <img
+            src="https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-859.jpg?size=338&ext=jpg&ga=GA1.1.1687694167.1711929600&semt=ais"
+            alt=""
+          />
+        </div>
+        <h2 className="text-3xl">Dharaka Meth Deshan</h2>
+        <p>Matara SL</p>
+
+        <div className="w-[150px] h-[40px] mt-10 flex justify-center items-center bg-blue-700 hover:bg-black text-white rounded-3xl cursor-pointer">
+          <button
+            type="submit"
+            onClick={() => logoutUser()}
+            className="text-white text-2xl font-bold font-mono"
+          >
+            Log Out
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
