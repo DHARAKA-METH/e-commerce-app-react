9870492b1f70eed829ae57d2555984ee8752b78d
DHARAKA DM <deshandm699@gmail.com>
2024-04-04 11:47:09 +0530

    profile data update

--- src/Pages/User/User.js
+++ src/Pages/User/User.js
@@ -1,17 +1,20 @@
+import { useSelector } from "react-redux";
 import logoutUser from "../../Utils/Auth/logoutUser";
 
 const User = () => {
+  const userData = useSelector((Store) => Store.UserSlice.user);
+ // console.log(userData)
   return (
     <div className="w-full overflow-y-scroll pt-[200px]">
       <div className=" flex flex-col items-center ">
         <div className="w-[200px] h-[200px] rounded-[50%]">
           <img
-            src="https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-859.jpg?size=338&ext=jpg&ga=GA1.1.1687694167.1711929600&semt=ais"
+            src={userData.profileImage}
             alt=""
           />
         </div>
-        <h2 className="text-3xl">Dharaka Meth Deshan</h2>
-        <p>Matara SL</p>
+        <h2 className="text-3xl">{userData.name}</h2>
+        <p>{userData.address}</p>
 
         <div className="w-[150px] h-[40px] mt-10 flex justify-center items-center bg-blue-700 hover:bg-black text-white rounded-3xl cursor-pointer">
           <button
